9b666cfeb9c3e60c867e8793bca96f81db586cf9
leonardosibela <leonardo_sibela@hotmail.com>
2018-03-02 00:57:25 +0000

    Project

---
+++ ProjetoSalario/Funcionario.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace ProjetoSalario
+{
+    class Funcionario
+    {
+        private static readonly int PORCENTAGEM_COMISSAO = 1;
+        private static readonly int PORCENTAGEM_IMPOSTO = 3;
+        private static readonly int VALOR_AJUDA_CUSTO = 200;
+        private static readonly int VALOR_VALE_TRANSPORTE = 50;
+
+        public string nome { get; set; }
+        public double salarioBruto { get; set; }
+        public double adicionais { get; private set; }
+        public double descontos { get; private set; }
+
+        private Boolean imposto;
+
+        public Boolean impostoProp
+        {
+            get { return imposto; }
+            set
+            {
+                this.imposto = value;
+                calcularDescontos();
+            }
+        }
+
+        private Boolean valeTransporte;
+
+        public Boolean valeTransporteProp
+        {
+            get { return this.valeTransporte; }
+            set
+            {
+                this.valeTransporte = value;
+                calcularDescontos();
+            }
+        }
+
+        private Boolean comissao;
+
+        public Boolean comissaoProp
+        {
+            get { return this.comissao; }
+            set
+            {
+                this.comissao = value;
+                calcularAdicionais();
+            }
+        }
+
+        private Boolean ajudaCusto;
+
+        public Boolean ajudaCustoProp
+        {
+            get { return ajudaCusto; }
+            set
+            {
+                this.ajudaCusto = value;
+                calcularAdicionais();
+            }
+        }
+
+        private double salarioLiquido;
+
+        public double salarioLiquidoProp
+        {
+            get { return this.salarioBruto + this.adicionais - this.descontos; }
+            set { salarioLiquido = value; }
+        }
+
+        private void calcularDescontos()
+        {
+            this.descontos = 0;
+
+            if (this.valeTransporte)
+            {
+                this.descontos += VALOR_VALE_TRANSPORTE;
+            }
+
+            if (this.imposto)
+            {
+                this.descontos += (this.salarioBruto / 100 * PORCENTAGEM_IMPOSTO);
+            }
+        }
+
+        private void calcularAdicionais()
+        {
+            this.adicionais = 0;
+
+            if (this.ajudaCusto)
+            {
+                this.adicionais += VALOR_AJUDA_CUSTO;
+            }
+
+            if (this.comissao)
+            {
+                this.adicionais += (this.salarioBruto / 100 * PORCENTAGEM_COMISSAO);
+            }
+        }
+    }
+}
---
+++ ProjetoSalario/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+
+namespace ProjetoSalario
+{
+    public partial class MainWindow : Window
+    {
+        List<Funcionario> funcionarios = new List<Funcionario>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            Funcionario funcionario = obterFuncionario();
+            funcionarios.Add(funcionario);
+            dtgFuncionarios.ItemsSource = null;
+            dtgFuncionarios.ItemsSource = funcionarios;
+
+            lblSarioFixo.Content = "R$ " + funcionario.salarioLiquidoProp.ToString("N", new CultureInfo("pt-BR"));
+        }
+
+        private Funcionario obterFuncionario()
+        {
+            Funcionario funcionario = new Funcionario();
+            funcionario.nome = txtNome.Text;
+            funcionario.salarioBruto = Double.Parse(txtSalarioBruto.Text);
+
+            funcionario.comissaoProp = rdbComissao.IsChecked.Value;
+            funcionario.ajudaCustoProp = rdbAjudaCusto.IsChecked.Value;
+            funcionario.impostoProp = chkImposto.IsChecked.Value;
+            funcionario.valeTransporteProp = chkValeTransporte.IsChecked.Value;
+
+            return funcionario;
+        }
+    }
+}
